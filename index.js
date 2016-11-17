6e7ae06fb607ea51e45a246d5b3f03cd123e6e68
WXiaoming <klamtine@gmail.com>
2016-11-17 17:35:07 +0800

    auto add eslint config

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 const path = require('path');
 const fs = require('fs');
+const PRECOMMIT_FILE = './pre-commit.txt';
+const ESLINT_FILE = './eslintrc.txt';
 
 let root = path.resolve(__dirname, '..', '..');
 let hookDir = path.resolve(root, '.git', './hooks');
 
-function readFile() {
+const ESLINT_FILE_TARGET = path.resolve(root, './.eslintrc');
+
+function readFile(filename) {
   return new Promise((resolve, reject) => {
-    fs.readFile('./pre-commit.txt', 'utf8', (err, data) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
       if (err) return reject(err);
       resolve(data);
     });
   });
 }
 
-function writeFile(raw) {
+function writeFile(raw, target) {
   return new Promise((resolve, reject) => {
-    fs.writeFile(`${hookDir}/pre-commit`, raw, { mode: '0755' }, (err) => {
+    fs.writeFile(target, raw, { mode: '0755' }, (err) => {
       if (err) return reject(err);
       resolve();
     });
   });
 }
 
-readFile()
-  .then(writeFile)
+function checkFile(target) {
+  return new Promise((resolve, reject) => {
+    fs.stat(target, (err) => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+}
+
+readFile(PRECOMMIT_FILE)
+  .then(raw => writeFile(raw, `${hookDir}/pre-commit`))
   .then(() => console.log('Git pre-commit set up success!'))
-  .catch(e => console.log(`Git pre-commit set up fail due to: ${e}`));
+  .catch(e => console.log(`Git pre-commit set up fail due to: ${e}`))
+
+checkFile(ESLINT_FILE_TARGET)
+  .catch(() => {
+    return readFile(ESLINT_FILE)
+      .then(raw => writeFile(raw, ESLINT_FILE_TARGET));
+  })
+  .then(() => console.log('Eslintrc set up success!'))
+  .catch(e => console.log(`Eslintrc set up fail due to: ${e}`));
